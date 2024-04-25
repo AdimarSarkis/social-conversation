ee73bdb4acc252da447be124f40d66a205f9c302
AdimarSarkis <sarkis170@gmail.com>
2024-04-24 23:03:00 -0300

    uploadthings adicionado

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        /*domains: [ 'uploadthing.com', "utfs.io"
+        ]*/
+        remotePatterns: [{
+            protocol: 'https',
+            hostname: "utfs.io"
+        }]
+    }
+};
 
 export default nextConfig;
